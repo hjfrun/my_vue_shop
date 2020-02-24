b7a89a8c247d3fc87fa8b4a7e26fb24f722646af
jianfeng.he <jianfeng.he@oliverwyman.com>
2020-02-24 16:46:09 +0800

    container

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,8 +1,11 @@
 <template>
-  <div>
-    Home Component
-    <el-button type="info" @click="logout">Logout</el-button>
-  </div>
+  <el-container class="home-container">
+    <el-header>Header<el-button type="info" @click="logout">Logout</el-button></el-header>
+    <el-container>
+      <el-aside width="200px">Aside</el-aside>
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
@@ -17,4 +20,19 @@ export default {
 </script>
 
 <style lang="less" scoped>
+.home-container {
+  height: 100%;
+}
+
+.el-header {
+  background-color: #373d41;
+}
+
+.el-aside {
+  background-color: #333744;
+}
+
+.el-main {
+  background-color: #eaedf1;
+}
 </style>
