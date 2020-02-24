6670a63374f05f9ebfaeda353a24aeee44947410
jianfeng.he <jianfeng.he@oliverwyman.com>
2020-02-24 16:31:00 +0800

    finishe login

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -4,16 +4,26 @@
       <div class="avatar_box">
         <img src="../assets/logo.png" alt />
       </div>
-      <el-form :model="loginForm" label-width="0px" class="login_form">
-        <el-form-item>
+      <el-form
+        ref="loginFormRef"
+        :model="loginForm"
+        :rules="loginFormRules"
+        label-width="0px"
+        class="login_form"
+      >
+        <el-form-item prop="username">
           <el-input v-model="loginForm.username" prefix-icon="iconfont icon-user"></el-input>
         </el-form-item>
-        <el-form-item>
-          <el-input v-model="loginForm.password" prefix-icon="iconfont icon-3702mima" type="password"></el-input>
+        <el-form-item prop="password">
+          <el-input
+            v-model="loginForm.password"
+            prefix-icon="iconfont icon-3702mima"
+            type="password"
+          ></el-input>
         </el-form-item>
         <el-form-item class="btns">
-          <el-button type="primary">Login</el-button>
-          <el-button type="info">Reset</el-button>
+          <el-button type="primary" @click="login">Login</el-button>
+          <el-button type="info" @click="resetLoginForm">Reset</el-button>
         </el-form-item>
       </el-form>
     </div>
@@ -22,12 +32,52 @@
 
 <script>
 export default {
-  data () {
+  data() {
     return {
       loginForm: {
-        username: '',
-        password: ''
+        username: 'admin',
+        password: '123456'
+      },
+      loginFormRules: {
+        username: [
+          {
+            required: true,
+            message: 'Please input user name',
+            trigger: 'blur'
+          },
+          {
+            min: 3,
+            max: 10,
+            message: 'Length between 3 and 10',
+            trigger: 'blur'
+          }
+        ],
+        password: [
+          { required: true, message: 'Please the password', trigger: 'blur' },
+          {
+            min: 6,
+            max: 15,
+            message: 'Length between 6 and 15',
+            trigger: 'blur'
+          }
+        ]
       }
+    }
+  },
+  methods: {
+    resetLoginForm() {
+      this.$refs.loginFormRef.resetFields()
+    },
+    login() {
+      this.$refs.loginFormRef.validate(async valid => {
+        if (!valid) return
+        const { data: res } = await this.$http.post('login', this.loginForm)
+        console.log(res)
+        if (res.meta.status !== 200) return this.$message.error('Login failed!')
+        this.$message.success('Login success!')
+        window.sessionStorage.setItem('token', res.data.token)
+        this.$router.push('/home')
+      })
     }
   }
 }
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '../components/Login.vue'
+import Home from '../components/Home.vue'
 
 Vue.use(VueRouter)
 
@@ -12,11 +13,22 @@ const routes = [
   {
     path: '/login',
     component: Login
+  },
+  {
+    path: '/home',
+    component: Home
   }
 ]
 
 const router = new VueRouter({
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  if (to.path === '/login') return next()
+  const tokenStr = window.sessionStorage.getItem('token')
+  if (!tokenStr) return next('/login')
+  next()
 })
 
 export default router
