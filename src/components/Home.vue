9ff258f6fec16fa445ff13077c622ea22db09eb1
jianfeng.he <jianfeng.he@oliverwyman.com>
2020-02-24 17:55:10 +0800

    add main containter

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,8 +1,31 @@
 <template>
   <el-container class="home-container">
-    <el-header>Header<el-button type="info" @click="logout">Logout</el-button></el-header>
+    <el-header>
+      <div>
+        <img src="../assets/heima.png" alt />
+        <span>电商后台管理系统</span>
+      </div>
+      <el-button type="info" @click="logout">退出</el-button>
+    </el-header>
     <el-container>
-      <el-aside width="200px">Aside</el-aside>
+      <el-aside width="200px">
+        <el-menu background-color="#333744" text-color="#fff" active-text-color="#ffd04b">
+          <el-submenu index="1">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>导航一</span>
+            </template>
+            <el-menu-item index="1-4-1">
+              <el-submenu index="1">
+                <template slot="title">
+                  <i class="el-icon-location"></i>
+                  <span>导航一</span>
+                </template>
+              </el-submenu>
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
       <el-main>Main</el-main>
     </el-container>
   </el-container>
@@ -10,7 +33,14 @@
 
 <script>
 export default {
+  created() {
+    this.getMenuList()
+  },
   methods: {
+    async getMenuList() {
+      const { data: res } = await this.$http.get('menus')
+      console.log('res: ', res)
+    },
     logout() {
       window.sessionStorage.clear()
       this.$router.push('/login')
@@ -26,6 +56,19 @@ export default {
 
 .el-header {
   background-color: #373d41;
+  display: flex;
+  justify-content: space-between;
+  padding-left: 0;
+  align-items: center;
+  color: white;
+  font-size: 20px;
+  > div {
+    display: flex;
+    align-items: center;
+    span {
+      margin-left: 15px;
+    }
+  }
 }
 
 .el-aside {
