f3b1cd80f8531fbdf8874b111de4a5ed4f54ac5e
Jianfeng He <jianfeng.he@oliverwyman.com>
2020-02-25 23:32:10 +0800

    add user form

--- src/components/user/Users.vue
+++ src/components/user/Users.vue
@@ -47,8 +47,21 @@
         :total="total"
       ></el-pagination>
     </el-card>
-    <el-dialog title="提示" :visible.sync="addDialogVisible" width="50%">
-      <span>这是一段信息</span>
+    <el-dialog title="添加用户" :visible.sync="addDialogVisible" width="50%">
+      <el-form :model="addForm" :rules="addFormRules" ref="addFormRef" label-width="70px">
+        <el-form-item label="用户名" prop="username">
+          <el-input v-model="addForm.username"></el-input>
+        </el-form-item>
+        <el-form-item label="密码" prop="password">
+          <el-input v-model="addForm.password"></el-input>
+        </el-form-item>
+        <el-form-item label="邮箱" prop="email">
+          <el-input v-model="addForm.email"></el-input>
+        </el-form-item>
+        <el-form-item label="手机" prop="mobile">
+          <el-input v-model="addForm.mobile"></el-input>
+        </el-form-item>
+      </el-form>
       <span slot="footer" class="dialog-footer">
         <el-button @click="addDialogVisible = false">取 消</el-button>
         <el-button type="primary" @click="addDialogVisible = false">确 定</el-button>
@@ -69,7 +82,55 @@ export default {
       },
       userList: [],
       total: 0,
-      addDialogVisible: false
+      addDialogVisible: false,
+      addForm: {
+        username: '',
+        password: '',
+        email: '',
+        mobile: ''
+      },
+      addFormRules: {
+        username: [
+          {
+            required: true,
+            message: '请输入用户名',
+            trigger: 'blur'
+          },
+          {
+            min: 3,
+            max: 10,
+            message: '用户名的长度在3到10个之间',
+            trigger: 'blur'
+          }
+        ],
+        password: [
+          {
+            required: true,
+            message: '请输入密码',
+            trigger: 'blur'
+          },
+          {
+            min: 6,
+            max: 15,
+            message: '用户名的长度在6到15个之间',
+            trigger: 'blur'
+          }
+        ],
+        email: [
+          {
+            required: true,
+            message: '请输入邮箱',
+            trigger: 'blur'
+          }
+        ],
+        mobile: [
+          {
+            required: true,
+            message: '请输入手机',
+            trigger: 'blur'
+          }
+        ]
+      }
     }
   },
   created() {
