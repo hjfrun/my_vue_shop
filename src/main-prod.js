fed1600712483336970d91cd70e19965cfe91abf
Jianfeng He <jianfeng.he@oliverwyman.com>
2020-03-01 10:25:38 +0800

    build entry for different env

---
+++ src/main-prod.js
@@ -0,0 +1,57 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import './plugins/element.js'
+
+import './assets/css/global.css'
+import './assets/fonts/iconfont.css'
+import TreeTable from 'vue-table-with-tree-grid'
+import VueQuillEditor from 'vue-quill-editor'
+
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
+
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
+
+import axios from 'axios'
+
+axios.defaults.baseURL = 'http://192.168.1.18:8888/api/private/v1/'
+axios.interceptors.request.use(config => {
+  // console.log('config: ', config)
+  NProgress.start()
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  return config
+})
+
+axios.interceptors.response.use(config => {
+  NProgress.done()
+  return config
+})
+
+Vue.prototype.$http = axios
+
+Vue.config.productionTip = false
+
+Vue.component('tree-table', TreeTable)
+
+// 将富文本编辑器，注册为全局可用组件
+Vue.use(VueQuillEditor)
+
+Vue.filter('dateFormat', originVal => {
+  const dt = new Date(originVal)
+  const y = dt.getFullYear()
+  const m = (dt.getMonth() + 1 + '').padStart(2, '0')
+  const d = (dt.getDate() + '').padStart(2, '0')
+
+  const hh = (dt.getHours() + '').padStart(2, '0')
+  const mm = (dt.getMinutes() + '').padStart(2, '0')
+  const ss = (dt.getSeconds() + '').padStart(2, '0')
+  return `${y}-${m}-${d} ${hh}:${mm}:${ss}`
+})
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
