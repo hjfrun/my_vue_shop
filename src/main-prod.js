251412ebdb101def7d75a872be6da27c5d5eaff2
Jianfeng He <jianfeng.he@oliverwyman.com>
2020-03-01 11:08:22 +0800

    html title

--- src/main-prod.js
+++ src/main-prod.js
@@ -1,19 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import './plugins/element.js'
 
 import './assets/css/global.css'
 import './assets/fonts/iconfont.css'
 import TreeTable from 'vue-table-with-tree-grid'
 import VueQuillEditor from 'vue-quill-editor'
 
-import 'quill/dist/quill.core.css'
-import 'quill/dist/quill.snow.css'
-import 'quill/dist/quill.bubble.css'
-
 import NProgress from 'nprogress'
-import 'nprogress/nprogress.css'
 
 import axios from 'axios'
 
